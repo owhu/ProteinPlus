39a47cef430f1a19e70db46449a7feadadbda183
oliver <oliverhu.oh@gmail.com>
2024-09-12 15:59:07 -0700

    project updated - edit upperbound view created to edit goal

---
+++ ProteinPlus/EditUpperBoundView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUpperBoundView.swift
+//  ProteinPlus
+//
+//  Created by Oliver Hu on 9/12/24.
+//
+
+import SwiftUI
+
+struct EditUpperBoundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUpperBoundView()
+}
