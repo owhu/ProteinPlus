22adb4fb109a5f6b624d9007c8f2fd2a3481a986
oliver <oliverhu.oh@gmail.com>
2024-09-20 09:46:40 -0700

    project updated

---
+++ ProteinPlus/StandardButtonModifier.swift
@@ -0,0 +1,8 @@
+//
+//  StandardButtonModifier.swift
+//  ProteinPlus
+//
+//  Created by Oliver Hu on 9/18/24.
+//
+
+import Foundation
---
+++ ProteinPlus/StandardSmallButtonModifier.swift
@@ -0,0 +1,8 @@
+//
+//  StandardSmallButtonModifier.swift
+//  ProteinPlus
+//
+//  Created by Oliver Hu on 9/18/24.
+//
+
+import Foundation
